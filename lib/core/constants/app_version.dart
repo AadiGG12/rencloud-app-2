ad64ae835c77dd31a1928a094a13e55ffc3adda3
RenCloud App <rencloud@users.noreply.github.com>
2026-07-30 17:08:07 +0000

    feat: native flutter_launcher_icons, roundIcon manifest integration & bump to v1.8.4

--- lib/core/constants/app_version.dart
+++ lib/core/constants/app_version.dart
@@ -2,8 +2,8 @@ import 'package:flutter/foundation.dart';
 import 'package:package_info_plus/package_info_plus.dart';
 
 class AppVersion {
-  static String _installedVersionName = '1.8.3';
-  static int _installedBuildNumber = 230;
+  static String _installedVersionName = '1.8.4';
+  static int _installedBuildNumber = 240;
 
   /// Read installed package info directly from native Android PackageManager BEFORE runApp()
   static Future<void> init() async {
